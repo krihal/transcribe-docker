b708ce64009661e31f3781f9509b5afade96c8a2
Kristofer Hallin <kristofer@sunet.se>
2025-09-15 14:07:00 +0200

    Updated build script.

--- build.sh
+++ build.sh
@@ -1,41 +1,41 @@
 #!/bin/sh
 
-# Verify that Git
+# Verify that Git is installed
 if ! command -v git &> /dev/null; then
 	echo "Git is not installed. Please install Git to proceed."
 	exit 1
 fi
 
+# Check if Docker is installed
 if command -v docker &> /dev/null ; then
 	docker=$(which docker)
 fi
 
+# Check if Podman is installed
 if command -v podman &> /dev/null ; then
 	docker=$(which podman)
 fi
 
-# Verify that Docker or Podman is installed
+# If neither Docker nor Podman is installed, exit with an error
 if [ -z "$docker" ]; then
 	echo "Neither Docker nor Podman is installed. Please install one of them to proceed."
 	exit 1
 fi
 
+# Check if Docker Compose is installed
 if ! command -v docker-compose &> /dev/null; then
 	echo "Docker Compose is not installed. Please install Docker Compose to proceed."
 	exit 1
 fi
 
 # Clone each of the components
-for i in transcribe-ui transcribe-backend transcribe-worker; do 
+for i in transcribe-ui transcribe-backend; do 
 	if [ -d "$i" ]; then
 		(cd $i && git pull)
 	else
 		(cd $i; git clone https://github.com/krihal/$i.git)
 	fi
-done
 
-# Build the Docker images
-for i in transcribe-ui transcribe-backend transcribe-worker; do
 	echo "Building Docker image for $i"
 	(cd $i; $docker build -f Dockerfile -t $i:latest .)
 done
